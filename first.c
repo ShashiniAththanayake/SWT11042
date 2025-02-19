899078bbac7da6626eb10bbd44691e3d45438cd6
ShashiniAththanayake <aththanayakeshashini3@gmail.com>
2025-02-19 09:56:36 +0530

    Added new file and updated second.c file

--- first.c
+++ first.c
@@ -2,6 +2,6 @@
 int main() {
     printf("Welcome to the Fundermental of Programming Session\n");
     printf("Shashini Aththanayake");
-    printf("SEU/IS/22/ICT/079");
+    printf("19/02/2025");
     return 0;
 }
